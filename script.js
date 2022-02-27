c226ba4606fad2d502e405439d7f67ea35507c77
Vandana Khatri <vandanakhatrii7@gmail.com>
2022-02-27 18:22:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var navbar = document.getElementById("navbar");
+        function showMenu(){
+            navbar.style.right="0";
+        }
+        function hideMenu(){
+            navbar.style.right="-300px";
+        }
+
