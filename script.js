d9d0713dabcfb9d366d4a47059e99f0c3f72bdd6
Vandana Khatri <vandanakhatrii7@gmail.com>
2022-03-01 17:36:41 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var navbar = document.getElementById("navbar");
-        function showMenu(){
-            navbar.style.right="0";
-        }
-        function hideMenu(){
-            navbar.style.right="-300px";
-        }
+function showMenu() {
+    navbar.style.right = "0";
+}
+function hideMenu() {
+    navbar.style.right = "-300px";
+}
 
+var MainImage = document.getElementById('MainImage');
+var smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function () {
+    MainImage.src = smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    MainImage.src = smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    MainImage.src = smallImg[2].src;
+}
+smallImg[3].onclick = function () {
+    MainImage.src = smallImg[3].src;
+}
